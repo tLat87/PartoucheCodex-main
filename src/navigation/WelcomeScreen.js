db2b66a1bcb99c2cc3cef734bc2a5d4258cd014b
t.latush <t.latush@t-latush.local>
2025-05-03 12:34:45 +0200

    fgcvhb

--- src/navigation/WelcomeScreen.js
+++ src/navigation/WelcomeScreen.js
@@ -3,15 +3,15 @@ import {View, Text, Button, TouchableOpacity, Image} from 'react-native';
 import CustomBtn from '../component/CustomBtn';
 
 const WelcomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: '#000',  padding: 16,paddingBottom: 100, justifyContent: 'space-around', alignItems: 'center' }}>
+  <View style={{ flex: 1, backgroundColor: '#000',  padding: 16, justifyContent: 'space-around', alignItems: 'center' }}>
     {/*<View style={{alignItems: 'center'}}>*/}
-      <Image source={require('../assets/img/Grou.png')} style={{width: '90%'}} resizeMode='contain'/>
+      <Image source={require('../assets/img/Grou.png')} style={{width: '90%', height: '50%'}} resizeMode='contain'/>
     {/*</View>*/}
     {/*<View style={{}}>*/}
       <Text style={{fontSize: 16, color: '#fff', alignSelf:'center'}}>
         Discover fragments of writings from great scientists. Read them and try to guess who might have been the author of these thoughts. Find inspiration? Save your favorite quotes in your archive!
       </Text>
-      <CustomBtn title='Continue' onPress={() => navigation.replace('Main')} stylesAdd={{alignSelf:'center',marginTop: 50}}/>
+      <CustomBtn title='Continue' onPress={() => navigation.replace('Main')} stylesAdd={{alignSelf:'center'}}/>
     {/*</View>*/}
   </View>
 );
